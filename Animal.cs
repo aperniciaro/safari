448d594bba551824e5954630233049b52fc359f1
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-18 13:56:27 -0400

    created database and added animal class

---
+++ Animal.cs
@@ -0,0 +1,11 @@
+namespace safari
+{
+  public class Animal
+  {
+    public int Id { get; set; }
+    public string Species { get; set; }
+    public int CountOfTimesSeen { get; set; }
+    public string LocationOfLastSeen { get; set; }
+
+  }
+}
